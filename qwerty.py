f6f2facf29387483ce44f38beaec7d30464154b8
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-11 16:40:31 +0000

    'new'

--- qwerty.py
+++ qwerty.py
@@ -50,4 +50,5 @@ def compute_lcm(x, y):
                                                                                                                                                                                     while(True):
                                                                                                                                                                                                if((greater % x == 0) and (greater % y == 0)):
                                                                                                                                                                                                    
-                                                                                                                                                                                                   
+                                                                                                                                                                                                   def compute_lcm(x, y):
+                                                                                                                                                                                                       
