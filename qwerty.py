479aca66d81789c8820ada4fa4798d7165f612b9
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-11 16:36:43 +0000

    'new'

--- qwerty.py
+++ qwerty.py
@@ -35,4 +35,5 @@ def compute_lcm(x, y):
                                                                                                                                                    
                                                                                                                                                       return lcm
                                                                                                                                                   
+                                                                                                                                                  num1 = 54
                                                                                                                                                   
