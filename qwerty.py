8ca9ea9a81ed0b616f5b2cb4b60f06c3398c28eb
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-11 16:34:54 +0000

    'new'

--- qwerty.py
+++ qwerty.py
@@ -22,4 +22,5 @@ def compute_lcm(x, y):
                                                                          def compute_lcm(x, y):
                                                                              
                                                                                 # choose the greater number
-                                                                                
+                                                                                   if x > y:
+                                                                                       
